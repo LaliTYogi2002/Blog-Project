c1a9411cc0046ca8308ba4f40026b48ccb269cb3
LaliTYogi2002 <91830326+LaliTYogi2002@users.noreply.github.com>
2025-03-25 09:03:18 +0530

    added Role Respository

--- src/main/java/com/example/Blog_Project/Repository/RoleRepository.java
+++ src/main/java/com/example/Blog_Project/Repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package com.example.Blog_Project.Repository;
 
-public interface RoleRepository {
+import com.example.Blog_Project.Model.Role;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    boolean existsByName(@NotBlank(message = "Role name cannot be empty") String name);
 }
