b78abcb1d19518e42599146403d3a5ba1415db4f
LaliTYogi2002 <91830326+LaliTYogi2002@users.noreply.github.com>
2025-03-25 09:02:05 +0530

    added Global Exception

--- src/main/java/com/example/Blog_Project/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Blog_Project/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.Blog_Project.Controller;
+package com.example.Blog_Project.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
